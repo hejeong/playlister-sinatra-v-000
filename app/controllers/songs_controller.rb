4da1d830552ea3364ad0333b2df126eb30e08178
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 17:48:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    puts @song.artist
-    #erb :'song/show'
+    
+    erb :'song/show'
   end
 end
