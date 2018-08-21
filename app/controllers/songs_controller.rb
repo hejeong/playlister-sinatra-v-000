59dad153de04f4267fdd232cb61afd57c6ae315a
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 17:21:21 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,7 @@ class SongsController < ApplicationController
     erb :'song/new'
   end
 
-  post '/songs/new' do
+  post '/songs' do
     song = Song.create(params["Name"])
     song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     redirect to "songs/#{song.slug}"
