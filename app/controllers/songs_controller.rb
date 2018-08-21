6e96a58ea2b5a49736fb3fe80f142dcc808bb322
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 18:12:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,8 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
     erb :'song/edit'
   end
 end
