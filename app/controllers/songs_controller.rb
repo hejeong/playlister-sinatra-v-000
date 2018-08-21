870c704b3886f3deb1b337928fc229027626025f
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 18:14:30 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,6 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.
