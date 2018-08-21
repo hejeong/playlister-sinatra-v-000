60d123daa197049d3c5ae632df4f3406276227f3
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 17:44:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     #@song.genre_ids = params[:genres]
     @song.save
-    puts @song.artist
+    puts @song.artist.name
     #redirect to "songs/#{@song.slug}"
   end
 
